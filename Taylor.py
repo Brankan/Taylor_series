7a0a30ff799c0797f8337faeebf35d6fce73d0f9
Brankan <60178858+Brankan@users.noreply.github.com>
2022-07-04 18:57:05 +0200

    Update Taylor.py

--- Taylor.py
+++ Taylor.py
@@ -2,31 +2,30 @@ from scipy import integrate
 import math
 import matplotlib.pyplot as plt
 from sympy import *
+
 x = Symbol('x')
 funkcja = input("Type function:")
 a = int(input('Input a:'))
 y = eval(funkcja)
 summa = y
-n=0
-
+n = 0
 
 while True:
     n += 1
     y = y.diff(x)
-    d = (y*((x-a)**n)/math.factorial(n))
-    if d ==0:
+    d = (y * ((x - a) ** n) / math.factorial(n))
+    if d == 0:
         break
-    if n==50:
+    if n == 50:
         break
     summa += d
 
-
-bruh=str(summa)
+bruh = str(summa)
 print(bruh)
 
-xarray=[]
-yarray=[]
-farray=[]
+xarray = []
+yarray = []
+farray = []
 for x in range(-100,100):
     if x == 0:
         x=1
@@ -38,9 +37,3 @@ plt.plot(xarray, yarray,label='x' )
 plt.plot(xarray, farray,label='teilor(x)')
 plt.axis([-100, 100, -100, 100])
 plt.show()
-
-
-
-
-
-
